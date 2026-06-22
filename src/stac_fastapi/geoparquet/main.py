93deaf8347688fb983a6b5e5bf96df1b88a9c74d
Michael Smith <michael.smith.erdc@gmail.com>
2026-06-22 15:58:39 -0400

    add static swagger files

--- src/stac_fastapi/geoparquet/main.py
+++ src/stac_fastapi/geoparquet/main.py
@@ -1,4 +1,8 @@
 import stac_fastapi.geoparquet.api
+from fastapi.staticfiles import StaticFiles
 
 api = stac_fastapi.geoparquet.api.create()
 app = api.app
+
+# Mount the static directory to serve local assets
+app.mount("/static", StaticFiles(directory="static"), name="static")
