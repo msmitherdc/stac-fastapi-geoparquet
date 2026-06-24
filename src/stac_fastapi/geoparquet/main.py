4a2ff2e5343a2f9ef2760170d01dc9ea7abdf2b7
Michael Smith <michael.smith.erdc@gmail.com>
2026-06-24 11:40:54 -0400

    set path

--- src/stac_fastapi/geoparquet/main.py
+++ src/stac_fastapi/geoparquet/main.py
@@ -1,8 +1,13 @@
+import os
 import stac_fastapi.geoparquet.api
 from fastapi.staticfiles import StaticFiles
 
 api = stac_fastapi.geoparquet.api.create()
 app = api.app
 
+# Resolve the absolute path to the static directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+static_dir = os.path.join(BASE_DIR, "static")
+
 # Mount the static directory to serve local assets
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
