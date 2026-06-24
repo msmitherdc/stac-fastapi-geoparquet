ec3d03df09cff45ab2de64aa8151fd36788e9728
Michael Smith <michael.smith.erdc@gmail.com>
2026-06-24 14:56:17 -0400

    get the app

--- src/stac_fastapi/geoparquet/main.py
+++ src/stac_fastapi/geoparquet/main.py
@@ -1,9 +1,12 @@
 import os
-from starlette.staticfiles import StaticFiles
+import stac_fastapi.geoparquet.api
+from fastapi.staticfiles import StaticFiles
+
+api = stac_fastapi.geoparquet.api.create()
+app = api.app
 
 # Example of how it might be dynamically resolving the path
 default_static_dir = os.path.join(os.path.dirname(__file__), "static")
 # Allow overriding via environment variable or default to /app/static if it exists
 static_dir = os.getenv("STATIC_DIR", "/app/static" if os.path.exists("/app/static") else default_static_dir)
-
 app.mount("/static", StaticFiles(directory=static_dir), name="static")
